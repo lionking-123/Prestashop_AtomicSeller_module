3c95f6550d9c7022ded73e2b9d94208261ac67c3
lionking-123 <69095724+lionking-123@users.noreply.github.com>
2021-10-24 01:02:05 +0300

    Get Content ready

--- atomicseller/atomicseller.php
+++ atomicseller/atomicseller.php
@@ -4,6 +4,32 @@ if (!defined('_PS_VERSION_')) {
 }
 
 class AtomicSeller extends Module {
+    public $name;
+    /** @var string */
+    public $version;
+    /** @var string */
+    public $author;
+    /** @var bool */
+    public $need_instance;
+    /** @var bool */
+    public $bootstrap;
+    /** @var string */
+    public $displayName;
+    /** @var string */
+    public $description;
+    /** @var string */
+    public $js_path;
+    /** @var string */
+    public $css_path;
+    /** @var string */
+    public $logo_path;
+    /** @var string */
+    public $module_path;
+    /** @var string Text to display when ask for confirmation on uninstall action */
+    public $confirmUninstall;
+    /** @var string */
+    private $templateFile;
+
     // Constructor
     public function __construct() {
         $this->name = 'atomicseller';
@@ -19,10 +45,21 @@ class AtomicSeller extends Module {
         $this->description = $this->l('Show the Order list and details and send details via Email.');
 
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+
+        if (!$this->_path) {
+            $this->_path = __PS_BASE_URI__ . 'modules/' . $this->name . '/';
+        }
+        $this->js_path = $this->_path . 'views/js/';
+        $this->css_path = $this->_path . 'views/css/';
+        $this->logo_path = $this->_path . 'logo.png';
+        $this->module_path = $this->_path;
     }
 
     // Module install function
     public function install() {
+        Configuration::updateValue('WS_TOKEN', 'MIJIUGTIUY976R976F42UV087JIYUUYVO8GYPGLIE2PAB');
+        Configuration::updateValue('WS_STOREKEY', 'MYSTORE');
+
         return (
             parent::install()
             && $this->installTab('AdminAtomicSeller', 'AtomicSeller')
@@ -31,6 +68,9 @@ class AtomicSeller extends Module {
 
     // Module uninstall function
     public function uninstall() {
+        Configuration::deleteByName('WS_TOKEN');
+        Configuration::deleteByName('WS_STOREKEY');
+
         if(parent::uninstall()) {
             $this->uninstallTab('AdminAtomicSeller');
 
@@ -45,7 +85,11 @@ class AtomicSeller extends Module {
         $tab = new Tab();
         $tab->active = 1;
         $tab->class_name = $tab_class;
-        $tab->name = $tab_name;
+        $tab->name = array();
+
+        foreach (Language::getLanguages(true) as $lang) {
+            $tab->name[$lang['id_lang']] = $tab_name;
+        }
         $tab->id_parent = (int)Tab::getIdFromClassName($parent);
         $tab->module = $this->name;
         $tab->icon = "shopping_basket";
@@ -66,8 +110,43 @@ class AtomicSeller extends Module {
         return false;
     }
 
+    // Load dependencies
+    public function loadAsset() {
+        $this->addJsDefList();
+
+        $cssAssets = [
+            $this->css_path . 'back.css',
+        ];
+
+        $javascriptAssets = [
+            $this->js_path . 'back.js',
+        ];
+
+        $this->context->controller->addCSS($cssAssets, 'all');
+        $this->context->controller->addJS($javascriptAssets);
+    }
+
+    // Add javascript parameter define
+    protected function addJsDefList() {
+        Media::addJsDef(array(
+            'psr_controller_atomicseller_url' => $this->context->link->getAdminLink('AdminAtomicSeller'),
+            'psr_controller_atomicseller' => 'AdminAtomicSeller',
+            'psr_lang' => (int) Configuration::get('PS_LANG_DEFAULT'),
+            'connection_error' => $this->trans('Oops... looks like an error occurred on WebService connection!', array(), 'Modules.AtomicSeller.Admin'),
+            'psre_success' => $this->trans('Configuration updated successfully!', array(), 'Modules.AtomicSeller.Admin'),
+        ));
+    }
+
     // Module's configuration page
     public function getContent() {
+        $this->loadAsset();
+
+        $this->context->smarty->assign(array(
+            'ws_token' => Configuration::get('WS_TOKEN'),
+            'ws_storekey' => Configuration::get('WS_STOREKEY'),
+            'logo_path' => $this->logo_path,
+        ));
+
         return $this->display(__FILE__, 'views/templates/admin/configure.tpl');
     }
 }
